4b6dfe3bcaf1db9dd1256eaa36fe67d8ffa437e6
Heng Chong Ming <heng_chong_ming@cpf.gov.sg>
2024-11-17 10:08:36 +0800

    Lab: Testing First

--- src/main/java/example/cashcard/CashCardApplication.java
+++ src/main/java/example/cashcard/CashCardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAcademyCashcardApplication {
+public class CashCardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringAcademyCashcardApplication.class, args);
+        SpringApplication.run(CashCardApplication.class, args);
     }
 
 }
